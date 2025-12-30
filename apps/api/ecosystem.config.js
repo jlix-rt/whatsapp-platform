a84fb9a678090dadfdaf173704ac032694baf036
José Reynoso <69912375+ReynosoTiu@users.noreply.github.com>
2025-12-30 00:34:37 -0600

    abc

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -1,21 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'whatsapp-api',
-      script: '/var/www/apps/whatsapp-api/index.js',
-      cwd: '/var/www/apps/whatsapp-api',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3333
-      },
+      name: "whatsapp-api",
+      script: "index.js",
+      cwd: "/var/www/apps/whatsapp-api",
+      env_file: ".env",
       instances: 1,
-      exec_mode: 'fork',
-      autorestart: true,
-      max_memory_restart: '500M',
-      error_file: '/var/log/pm2/whatsapp-api-error.log',
-      out_file: '/var/log/pm2/whatsapp-api-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+      exec_mode: "fork"
     }
   ]
 };
-  
